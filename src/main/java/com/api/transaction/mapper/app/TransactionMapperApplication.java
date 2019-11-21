c18cef0e372c03ec577f49f99192a1458c18c8b0
DhananjayDjk <dj.iiitc@gmail.com>
2019-11-21 16:55:51 +0530

    added test class methods

--- src/main/java/com/api/transaction/mapper/app/TransactionMapperApplication.java
+++ src/main/java/com/api/transaction/mapper/app/TransactionMapperApplication.java
@@ -1,9 +1,15 @@
 package com.api.transaction.mapper.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.api.transaction.mapper"})
 public class TransactionMapperApplication {
 	
 	public static void main(String[] args) {
-		
+		SpringApplication.run(TransactionMapperApplication.class, args);
 	}
 	
 
--- src/test/java/com/api/transaction/mapper/service/test/TransactionMapperServiceNegativeScenarioTest.java
+++ src/test/java/com/api/transaction/mapper/service/test/TransactionMapperServiceNegativeScenarioTest.java
@@ -1,5 +1,21 @@
 package com.api.transaction.mapper.service.test;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 public class TransactionMapperServiceNegativeScenarioTest {
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@Test
+	public void testOpenBankApiServiceCall() {
+		
+	}
 
 }
